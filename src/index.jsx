d6ba422bebd1a5364cfe92385079b045f70deaa4
D4ntae <danko.delimar@gmail.com>
2023-11-16 22:22:59 +0100

    initial

---
+++ src/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+import { Index } from "./screens/Index";
+import HousingAdminView from "./screens/HousingAdminView";
+import UserAdminView from "./screens/UserAdminView";
+import TransportAdminView from "./screens/TransportAdminView";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Index />
+    },
+    {
+        path: "/users",
+        element: <UserAdminView />
+    },
+    {
+        path: "/transport",
+        element: <TransportAdminView />
+    },
+    {
+        path: "/housing",
+        element: <HousingAdminView />
+    }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
